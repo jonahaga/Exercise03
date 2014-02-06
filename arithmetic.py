3f3817dd3b266370ff8e49fa7a278e754a8af0ae
Jona Haga-Camacho <Jona@Flexo.local>
2014-02-05 18:59:30 -0800

    Removed duplicate code that was commented out.

--- arithmetic.py
+++ arithmetic.py
@@ -1,30 +1,3 @@
-# def add(num1, num2):
-#     return num1 + num2
-
-# def subtract(num1, num2):
-#     return num1 - num2
-
-# def multiply(num1, num2):
-#     return num1 * num2
-
-# def divide(num1, num2):
-#     if num2 == 0:
-#       print "Sorry. Can't divide by zero."
-#     else:
-#       return num1 / num2
-
-# def square(num1):
-#     return num1 ** 2
-
-# def cube(num1):
-#     return num1 ** 3
-
-# def power(num1, num2):
-#     return num1 ** num2
-
-# def mod(num1, num2):
-#     return num1 % num2
-
 def add(num1, num2):
     return num1 + num2
 
